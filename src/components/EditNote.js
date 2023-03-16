86080457a040378987770fe5ea15d51672334878
jogopin <jonn.g.pineda@gmail.com>
2023-03-16 16:48:17 +0100

    Update EditNote functionality & add basic css

--- src/components/EditNote.js
+++ src/components/EditNote.js
@@ -1,23 +1,25 @@
+import "./EditNote.css"
 import axios from "axios"
 import { useEffect, useState } from "react"
 
 export default function EditNote(props){
 
     const {boardId,noteId,setSelectedNote, getNotesList} = props
-    const [note,setNote] =useState({
-        title:"",
-        description:"",
-        checklist:[{task:"",isChecked:false}],
-        
-    })
-
+    
+    const [title,setTitle]=useState("")
+    const [description,setDescription]= useState("")
+    const [checklist, setChecklist]=useState(null)
 
     useEffect(()=>{
         
         axios.get(`${process.env.REACT_APP_API_URL}/api/notes/${boardId}/${noteId}`)
-            .then(responseAxios=>{
+            .then(res=>{
                 
-                setNote(responseAxios.data)
+                
+                setTitle(res.data.title)
+                setDescription(res.data.description)
+                setChecklist(res.data.checklist)
+
             })
             .catch(err=>{
                 console.log(`error getting the note`,err)
@@ -26,8 +28,10 @@ export default function EditNote(props){
 
     const handleSubmit = (e) => {
         e.preventDefault()
-
-        axios.put(`${process.env.REACT_APP_API_URL}/api/notes/${boardId}/${noteId}`,note)
+        
+        const noteUpdated = {title,description,checklist}
+        
+        axios.put(`${process.env.REACT_APP_API_URL}/api/notes/${boardId}/${noteId}`,noteUpdated)
           .then(responseAxios=>{
             console.log(`updated correctly`,responseAxios.data)
           })
@@ -36,56 +40,106 @@ export default function EditNote(props){
           })
           .finally(()=>{
             getNotesList()
+            setSelectedNote(null)
           })
         
     }
-    const handleOnChange = (e) => {
-        e.preventDefault()
+   
+    const handleOnChangeChecklist = (e,i) =>{
+      
+      const{name,value,checked} = e.target
 
-        setNote(prevState=>{
-            
-            return{...prevState, [e.target.name]:e.target.value}
-        })
+      //there are some errors when i checked the box, need to research more on that
+      setChecklist(prevState=>{
+        
+        const newChecklist=[...prevState]
 
+        newChecklist[i]={...checklist[i], [name]: name === "isChecked" ? checked : value}
+        return newChecklist
+        
+      })
+    }
+
+    const handlePlusClick =(e)=>{
+      e.preventDefault()
+
+      setChecklist(prevState=>{
+        return [...prevState, {task:"",isChecked:false} ]
+        
+      })
+    }
+    
+    const handleDeleteTask = (e,i)=>{
+      e.preventDefault()
+
+      setChecklist(prevState=>{
+
+        const newChecklist= [...prevState]
+        newChecklist.splice(i,1)
+               
+        return newChecklist
+      })
     }
 
     const renderNote = () => {
         
       return (
         <div className="note-details">
-          <form onSubmit={handleSubmit}>
+          <form className="note-form" onSubmit={handleSubmit}>
+            
             <input
               type="text"
               name="title"
               autoComplete="off"
-              value={note.title}
-              onChange={handleOnChange}
+              value={title}
+              onChange={(e)=>{setTitle(e.target.value)}}
               required
             />
+
+            
             <textarea
               rows={2}
               name="description"
               autoComplete="off"
-              value={note.description}
-              onChange={handleOnChange}
+              value={description}
+              onChange={(e)=>{setDescription(e.target.value)}}
             />
+
+            <div className="checklist container" >
+
+            </div>
+            {checklist ? checklist.map((taskObj,index)=>{
+              return<div className="task-container" key={index}>
+                <input type="checkbox" name="isChecked" checked={taskObj.isChecked}  onChange={(e)=>{handleOnChangeChecklist(e,index)}}/>
+                <input type="text" name="task" value={taskObj.task} onChange={(e)=>{handleOnChangeChecklist(e,index)}}/>
+                <button onClick={(e)=>{handleDeleteTask(e,index)}}>x</button>
+              </div>
+            }): ""}
+           
+              <button onClick={handlePlusClick}>+</button>
+
+          
+                
+
+            
             <button>done</button>
           </form>
 
           <button
+            className="btn-x"
             onClick={(e) => {
               e.preventDefault()
               setSelectedNote(null);
             }}
           >
-            X
+            -
           </button>
         </div>
       );
     };
 
     return <>
-        {note ? renderNote() : ""}
+        {renderNote()}
     </> 
 
 }
