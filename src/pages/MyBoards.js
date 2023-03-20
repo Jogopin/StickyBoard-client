e80bb9f38f7c95646f9ce095ddb9ac1bc638f847
jogopin <jonn.g.pineda@gmail.com>
2023-03-20 11:55:27 +0100

    Add delete  functionality for boards & basic Css

--- src/pages/MyBoards.js
+++ src/pages/MyBoards.js
@@ -1,3 +1,4 @@
+import "./MyBoards.css"
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { Link, Outlet } from "react-router-dom"
@@ -24,17 +25,44 @@ export default function MyBoards(){
         })  
       
       }
+    
+    const handleDelete =(boardId)=>{
+
+      axios.delete(`${API_URL}/api/boards/${boardId}`)
+        .then(responseAxios=>{
+          console.log(`board and its notes deleted`,responseAxios)
+        })
+        .catch(err=>{
+          console.log("error deleting the board and its notes",err)
+        })
+        .finally(()=>{
+          getMyBoards()
+        })
       
+    }
+
+    const handleEdit =(boardId)=>{
+      console.log(`board ${boardId} edit`)
+    }
+
     const renderMyBoardsList= ()=>{
         return (
-          <div>
+          <div className="miniboards-container">
             {myBoardsList.map((board) => (
-              
-                <Link to={`/myboards/${board._id}`} key={board._id}>
-                  <div >
+
+                <div className="miniboard" key={board._id}>
+                  
                     <h2>{board.name}</h2>
-                  </div>
-                </Link>
+                 
+                  <Link to={`/myboards/${board._id}`} >
+                  <button><b>...</b></button>
+                  </Link>
+                  <button onClick={()=>{handleEdit(board._id)}}>edit</button>
+                  <button onClick={()=>{handleDelete(board._id)}}>delete</button>
+
+                </div>
+           
+              
              
             ))}
             {/* <Outlet/> */}
