0cbff43fe7db442a1c59826d3940c63405bdd698
jogopin <jonn.g.pineda@gmail.com>
2023-04-05 16:53:37 +0200

    Add login,signup & logout to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,28 @@
+import { useContext } from "react";
 import "./NavBar.css";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
 
-export default function NavBar(){
-    return<>
-        <nav>
-            <NavLink to="/">HomePage</NavLink>
+export default function NavBar() {
+  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+
+  return (
+    <>
+      <nav>
+        <NavLink to="/">HomePage</NavLink>
+
+        {isLoggedIn ? (
+          <>
             <NavLink to="/myboards">My Boards</NavLink>
-        </nav>
+            <button onClick={logOutUser}>Logout</button>
+          </>
+        ) : (
+          <>
+            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/signup">Signup</NavLink>
+          </>
+        )}
+      </nav>
     </>
+  );
 }
