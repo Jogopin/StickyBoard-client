0df97fd5b5d6bfbd94a5141ae8e31a894f6c02da
jogopin <jonn.g.pineda@gmail.com>
2023-04-05 16:52:56 +0200

    Add LoginPage  & auth.context

---
+++ src/context/auth.context.js
@@ -0,0 +1,75 @@
+// src/context/auth.context.js
+
+import { useState, useEffect, createContext } from "react";
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+const AuthContext = createContext();
+
+function AuthProviderWrapper(props) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  const storeToken = (token) => {
+    localStorage.setItem("authToken", token);
+  };
+
+  const authenticateUser = () => {
+    const storedToken = localStorage.getItem("authToken");
+
+    if (storedToken) {
+      axios
+        .get(`${API_URL}/auth/verify`, {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        })
+        .then((response) => {
+          // If the server verifies that the JWT token is valid
+          const user = response.data;
+
+          setIsLoggedIn(true);
+          setIsLoading(false);
+          setUser(user);
+        })
+        .catch((error) => {
+          // If the server sends an error response (invalid token)
+          // Update state variables
+          setIsLoggedIn(false);
+          setIsLoading(false);
+          setUser(null);
+        });
+    } else {
+      // If the token is not available (or is removed)
+      setIsLoggedIn(false);
+      setIsLoading(false);
+      setUser(null);
+    }
+  };
+
+  const removeToken = () => {                    // <== ADD
+    // Upon logout, remove the token from the localStorage
+    localStorage.removeItem("authToken");
+  }
+
+  const logOutUser = () => {                   // <== ADD    
+    // To log out the user, remove the token
+    removeToken();
+    // and update the state variables    
+    authenticateUser();
+  }
+
+  useEffect(() => {
+    authenticateUser()
+  });
+
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn, isLoading, user, storeToken, authenticateUser, logOutUser }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
+
+export { AuthProviderWrapper, AuthContext };
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,75 @@
+import { useContext, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+
+function LoginPage(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  
+  const navigate = useNavigate();
+
+  const { storeToken, authenticateUser } = useContext(AuthContext)
+
+  const handleUsername = (e) => setUsername(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+
+  
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    const requestBody = { username, password };
+ 
+    axios.post(`${API_URL}/auth/login`, requestBody)
+      .then((response) => {
+                
+        storeToken(response.data.authToken);  
+        
+        // Verify the token by sending a request 
+        // to the server's JWT validation endpoint. 
+        authenticateUser()
+        
+      
+        navigate('/');
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      })
+  };
+  
+  return (
+    <div className="LoginPage">
+      <h1>Login</h1>
+
+      <form onSubmit={handleLoginSubmit}>
+        <label>Username:</label>
+        <input 
+          type="username"
+          name="username"
+          value={username}
+          onChange={handleUsername}
+        />
+
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePassword}
+        />
+
+        <button type="submit">Login</button>
+      </form>
+      { errorMessage && <p className="error-message">{errorMessage}</p> }
+
+      <p>Don't have an account yet?</p>
+      <Link to={"/signup"}> Sign Up</Link>
+    </div>
+  )
+}
+
+export default LoginPage;
